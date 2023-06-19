a1b546f1319ce884bf3980da21921700c976408a
Lizzie <leejihye7117@gmail.com>
2023-06-19 12:43:57 +0900

    theme 수정

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./pages/App.tsx";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { ThemeProvider } from "@mui/material";
+import { CssBaseline, ThemeProvider } from "@mui/material";
 import theme from "./style/theme.ts";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Quizzes from "./pages/Quizzes.tsx";
@@ -35,8 +35,11 @@ const router = createBrowserRouter([
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
-      <RouterProvider router={router} />
-      <ThemeProvider theme={theme}>{/* <App /> */}</ThemeProvider>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <RouterProvider router={router} />
+        {/* <App /> */}
+      </ThemeProvider>
     </QueryClientProvider>
   </React.StrictMode>
 );
--- src/style/theme.ts
+++ src/style/theme.ts
@@ -1,17 +1,13 @@
 import { createTheme } from "@mui/material/styles";
-import { red } from "@mui/material/colors";
 
 // Create a theme instance.
 const theme = createTheme({
   palette: {
-    primary: {
-      main: "#556cd6",
+    background: {
+      default: "#000000",
     },
-    secondary: {
-      main: "#19857b",
-    },
-    error: {
-      main: red.A400,
+    text: {
+      primary: "#ffffff",
     },
   },
 });
